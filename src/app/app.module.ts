96fbdceeffff1a1702a3c60de1bd90b253a5f2d2
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2023-04-21 15:15:24 +0100

    implementing the employee component
    implementing the employee component in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { Employee } from './employees/shared/employee.model';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule} from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import {FormsModule} from "@angular/forms"
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent, 
+
+    
+  ], 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBHEJyIPzBW1eRXuigEUljP5-jaalwNXC8",
+        authDomain: "crud-app-9a1d4.firebaseapp.com",
+        projectId: "crud-app-9a1d4",
+        storageBucket: "crud-app-9a1d4.appspot.com",
+        messagingSenderId: "1027740494191",
+        appId: "1:1027740494191:web:21c3d5a3397394e932e09a",
+        measurementId: "G-2P169VE5P8"
+    }
+};
